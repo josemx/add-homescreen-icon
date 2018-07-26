703977e92dd6bc9884e55d540e880bd30f92d88b
Jose Martinez <jxse.mx@gmail.com>
2018-07-25 19:17:18 -0500

    inital version working

--- gulpfile.js
+++ gulpfile.js
@@ -10,56 +10,56 @@ var replace = require('gulp-replace');
 var fs      = require('fs');
 
 gulp.task('minifyHTML', function () {
-  return gulp.src('components/*.html')
+  return gulp.src('src/*.html')
     .pipe(rename(function (path) {
       path.basename += '.min';
     }))
     .pipe(minHTML())
-    .pipe(gulp.dest('components/min'));
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('processAndMinCSS', function () {
-  return gulp.src('components/*.css')
+  return gulp.src('src/*.css')
     .pipe(replace(/%/g, '%25'))
     .pipe(rename(function (path) {
       path.basename += '.min';
     }))
     .pipe(minCSS())
-    .pipe(gulp.dest('components/min'))
+    .pipe(gulp.dest('dist'))
 });
 
 gulp.task('replaceAndMinBKMT', function () {
-  return gulp.src('components/*.js')
+  return gulp.src('src/*.js')
   .pipe(replace(/\/\*gulp-replace-html\*\//, function (s) {
-    return fs.readFileSync('components/min/ahi.min.html', 'utf8');
+    return fs.readFileSync('dist/index.min.html', 'utf8');
   }))
   .pipe(replace(/\/\*gulp-replace-css\*\//, function (s) {
-    return fs.readFileSync('components/min/ahi.min.css', 'utf8');
+    return fs.readFileSync('dist/main.min.css', 'utf8');
   }))
   .pipe(rename(function (path) {
     path.basename += '.bkmt.min';
   }))
   .pipe(minJS())
-  .pipe(gulp.dest('components/min'));
+  .pipe(gulp.dest('dist'));
 })
 
 gulp.task('minifyJS', function () {
-  return gulp.src('components/*.js')
+  return gulp.src('src/*.js')
     .pipe(rename(function (path) {
       path.basename += '.min';
     }))
     .pipe(minJS())
-    .pipe(gulp.dest('components/min'));
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('htmlFriendly', function () {
-  return gulp.src('components/min/*.bkmt.min.js')
+  return gulp.src('dist/*.bkmt.min.js')
   .pipe(replace(/</, '&lt;'))
   .pipe(replace(/>/, '&gt;'))
   .pipe(rename(function (path) {
     path.basename += '.ht.min';
   }))
-  .pipe(gulp.dest('components/min'));
+  .pipe(gulp.dest('dist'));
 });
 
 
