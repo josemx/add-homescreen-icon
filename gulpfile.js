948f53d840688e3117d3617a4f54198c8380d276
Jose Martinez <jxse.mx@gmail.com>
2018-07-25 20:03:18 -0500

    simplifying gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -9,58 +9,37 @@ var rename  = require('gulp-rename');
 var replace = require('gulp-replace');
 var fs      = require('fs');
 
+const renameConfig = {
+  suffix: '.min',
+};
+
 gulp.task('minifyHTML', function () {
-  return gulp.src('src/*.html')
-    .pipe(rename(function (path) {
-      path.basename += '.min';
-    }))
+  return gulp.src('src/index.html')
+    .pipe(rename(renameConfig))
     .pipe(minHTML())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist/min'));
 });
 
-gulp.task('processAndMinCSS', function () {
-  return gulp.src('src/*.css')
+gulp.task('processAndMinifyCSS', function () {
+  return gulp.src('src/main.css')
     .pipe(replace(/%/g, '%25'))
-    .pipe(rename(function (path) {
-      path.basename += '.min';
-    }))
+    .pipe(rename(renameConfig))
     .pipe(minCSS())
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('dist/min'))
 });
 
-gulp.task('replaceAndMinBKMT', function () {
-  return gulp.src('src/*.js')
-  .pipe(replace(/\/\*gulp-replace-html\*\//, function (s) {
-    return fs.readFileSync('dist/index.min.html', 'utf8');
-  }))
-  .pipe(replace(/\/\*gulp-replace-css\*\//, function (s) {
-    return fs.readFileSync('dist/main.min.css', 'utf8');
-  }))
-  .pipe(rename(function (path) {
-    path.basename += '.bkmt.min';
-  }))
-  .pipe(minJS())
-  .pipe(gulp.dest('dist'));
+gulp.task('replaceAndMinifyBookmarklet', ['minifyHTML', 'processAndMinifyCSS'], function () {
+  return gulp.src('src/index.js')
+    .pipe(replace(/\/\*gulp-replace-html\*\//, function (s) {
+      return fs.readFileSync('dist/min/index.min.html', 'utf8');
+    }))
+    .pipe(replace(/\/\*gulp-replace-css\*\//, function (s) {
+      return fs.readFileSync('dist/min/main.min.css', 'utf8');
+    }))
+    .pipe(rename(renameConfig))
+    .pipe(minJS())
+    .pipe(gulp.dest('dist/min'));
 })
 
-gulp.task('minifyJS', function () {
-  return gulp.src('src/*.js')
-    .pipe(rename(function (path) {
-      path.basename += '.min';
-    }))
-    .pipe(minJS())
-    .pipe(gulp.dest('dist'));
-});
 
-gulp.task('htmlFriendly', function () {
-  return gulp.src('dist/*.bkmt.min.js')
-  .pipe(replace(/</, '&lt;'))
-  .pipe(replace(/>/, '&gt;'))
-  .pipe(rename(function (path) {
-    path.basename += '.ht.min';
-  }))
-  .pipe(gulp.dest('dist'));
-});
-
-
-gulp.task('default', ['minifyHTML', 'processAndMinCSS', 'replaceAndMinBKMT']);
+gulp.task('default', ['minifyHTML', 'processAndMinifyCSS', 'replaceAndMinifyBookmarklet']);
